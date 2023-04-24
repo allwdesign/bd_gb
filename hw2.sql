80ed8d0f09c1e96fa7ff7cc8bf902276ce3d1ba0
Natalia <31250744+allwdesign@users.noreply.github.com>
2023-04-24 22:30:34 +0300

    Del trailling space

--- hw2.sql
+++ hw2.sql
@@ -3,14 +3,14 @@ USE lesson2;
 # id, order_date, count_product
 CREATE TABLE IF NOT EXISTS sales
 (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     order_date DATE NOT NULL, 
     count_product INT NOT NULL
 );
 
 INSERT INTO sales (order_date, count_product)
 VALUES
-	(STR_TO_DATE('2022-01-01', '%Y-%m-%d'), 156),
+    (STR_TO_DATE('2022-01-01', '%Y-%m-%d'), 156),
     (STR_TO_DATE('2022-01-02', '%Y-%m-%d'), 180),
     (STR_TO_DATE('2022-01-03', '%Y-%m-%d'), 21),
     (STR_TO_DATE('2022-01-04', '%Y-%m-%d'), 124),
@@ -19,10 +19,10 @@ VALUES
 SELECT * FROM sales;
 
 /*
-	2. Для данных таблицы “sales” укажите тип заказа в зависимости от кол-ва : 
-	меньше 100  -    Маленький заказ
-	от 100 до 300 - Средний заказ
-	больше 300  -     Большой заказ
+    2. Для данных таблицы “sales” укажите тип заказа в зависимости от кол-ва : 
+    меньше 100  -    Маленький заказ
+    от 100 до 300 - Средний заказ
+    больше 300  -     Большой заказ
 */
 SELECT id AS 'id заказа',
 CASE
@@ -44,20 +44,20 @@ AS 'Тип заказа'
 FROM sales;
 
 /*
-	3. Создайте таблицу “orders”, заполните ее значениями. 
+    3. Создайте таблицу “orders”, заполните ее значениями. 
     Покажите “полный” статус заказа, используя оператор CASE
 */
 CREATE TABLE IF NOT EXISTS orders
 (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     employee_id VARCHAR(255) NOT NULL, 
     amount DECIMAL(5,2) NOT NULL,
-	order_status VARCHAR(20) NOT NULL
+    order_status VARCHAR(20) NOT NULL
 );
 
 INSERT INTO orders (employee_id, amount, order_status)
 VALUES
-	('e03', 15.00, 'OPEN'),
+    ('e03', 15.00, 'OPEN'),
     ('e01', 25.50, 'OPEN'),
     ('e05', 100.70, 'CLOSED'),
     ('e02', 22.18, 'OPEN'),
